ce537da1f11f28565bf061ff0cacf8529ef4d6b8
Yvonne Lip Lim <164453695+YvonneLipLim@users.noreply.github.com>
2024-09-19 15:40:04 +0800

    Update 06. Conditional Statement Using For Loops Practice.py

--- 06. Conditional Statement Using For Loops Practice.py
+++ 06. Conditional Statement Using For Loops Practice.py
@@ -1,6 +1,6 @@
 '''
 Q1.
-Create usernames from a given list of names. The usernames should be lowercases versions of the names with all spaces replaced by underscores. Add the email domain of @abc.com to the usernames too.
+Create usernames from a given list of names. The usernames should be lowercase versions of the names with all spaces replaced by underscores. Add the email domain of @abc.com to the usernames too.
 Katharine Hepburn
 Meryl Streep
 Jack Nicholson
@@ -11,23 +11,25 @@ Walter Brennan
 Bette Davis
 Spencer Tracy
 Marlon Brando
+'''
 
 names =["Katharine Hepburn", "Meryl Streep", "Jack Nicholson", "Ingrid Bergman", "Daniel Day-Lewis", "Frances McDormand", "Walter Brennan", "Bette Davis", "Spencer Tracy", "Marlon Brando"]
 usernames = []
 
 for name in names: #iterates over each name in the names list
-    lowercase_name = name.lower().replace(" ", "_") #converts the name to lowercase aqnd replaces all spaces in the name with underscores
+    lowercase_name = name.lower().replace(" ", "_") #converts the name to lowercase and replaces all spaces in the name with underscores
     email = lowercase_name + "@abc.com" #converts lowercase name to email address
     usernames.append(email) #adds the email domain to the usernames
 print(usernames)
 
 ['katharine_hepburn@abc.com', 'meryl_streep@abc.com', 'jack_nicholson@abc.com', 'ingrid_bergman@abc.com', 'daniel_day-lewis@abc.com', 'frances_mcdormand@abc.com', 'walter_brennan@abc.com', 'bette_davis@abc.com', 'spencer_tracy@abc.com', 'marlon_brando@abc.com']
-'''
+-----------------------------------------------------------------------------------------------------------------------
 
 
 '''
-Q2. Given the input name list as below, modify it to a usernames list with everything lowercase and replaces spaces with underscores running in for loop over the list.
+Q2. Given the input name list as below, modify it to a usernames list with everything lowercase and replace spaces with underscores running in for loop over the list.
 names =["Katharine Hepburn", "Meryl Streep", "Jack Nicholson", "Ingrid Bergman", "Daniel Day-Lewis", "Frances McDormand", "Walter Brennan", "Bette Davis", "Spencer Tracy", "Marlon Brando"]
+'''
 
 usernames = ["Katharine Hepburn", "Meryl Streep", "Jack Nicholson", "Ingrid Bergman", "Daniel Day-Lewis", "Frances McDormand", "Walter Brennan", "Bette Davis", "Spencer Tracy", "Marlon Brando"]
 for names in range(len(usernames)): #This loop structure allows access to both the index i and the element at that index in the list.
@@ -35,12 +37,10 @@ for names in range(len(usernames)): #This loop structure allows access to both t
 print(usernames)
 
 ['joey_tribbiani', 'monica_geller', 'chandler_bing', 'phoebe_buffay']
-'''
+-----------------------------------------------------------------------------------------------------------------------
 
 
-'''
-Q3.
-Create a for loop that iterates over a list of strings, tokens, and counts how many of them are XML tags(opens in a new tab). Keep track of the number of tags using the variable count.
+#Q3. Create a for loop that iterates over a list of strings, tokens, and counts how many of them are XML tags(opens in a new tab). Keep track of the number of tags using the variable count.
 
 tokens = ["<html>", "Hello", "<body>", "Welcome", "</body>", "</html>"]
 count = 0 #To keep track of the number of XML tags begin with 0
@@ -51,7 +51,7 @@ for token in tokens: #iterates over each string in the tokens list
 print(count)
 
 4
-'''
+-----------------------------------------------------------------------------------------------------------------------
 
 
 '''
@@ -62,6 +62,7 @@ For example, if the list is items = ['first string', 'second string'], printing 
 <li>first string</li>
 <li>second string</li>
 </ul>
+'''
 
 items = ['Hello World', 'Welcome 2024']
 html_str = "<ul>\n"
@@ -75,6 +76,6 @@ print(html_str)
 <li>Hello World</li>
 <li>Welcome 2024</li>
 </ul>
-'''
+
 
 
