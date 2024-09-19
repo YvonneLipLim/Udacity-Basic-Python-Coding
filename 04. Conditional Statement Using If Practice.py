6bf277cbd42828721ff04046f16ee2d93eba308f
Yvonne Lip Lim <164453695+YvonneLipLim@users.noreply.github.com>
2024-09-19 15:35:13 +0800

    Update 04. Conditional Statement Using If Practice.py

--- 04. Conditional Statement Using If Practice.py
+++ 04. Conditional Statement Using If Practice.py
@@ -6,6 +6,7 @@ In the conditional statement, assign the result variable in a string message bas
 51 - 150: Protein Bar
 151 - 180: Wafer Mint
 181 - 200: Box of Chocolate
+'''
 
 points = 90 #Use this to run your code
 if points <= 50:
@@ -19,12 +20,13 @@ else:
 print(result)
 
 Congratulations! You won a Protein Bar!
-'''
+-----------------------------------------------------------------------------------------------------------------
 
 
 '''
 Q2. Create a conditional statement to inform user about how their guess compares to the answer. Inform the user if their guess is too high or too low. 
 Use looop to allow users to make multiple guesses until they guess the correct answer.
+'''
 
 answer = 30 #provide answer
 guess = 51 #provide guess
@@ -43,7 +45,7 @@ print(result)
 Guess the number:  35
 Guess the number:  30
 Nice!  Your guess matched the answer!
-
+-----------------------------------------------------------------------------------------------------------------
 
 '''
 Q3. How much will a person need to pay in total, including state and local sales tax, for a purchase made in the following respective States where an individual is from?
@@ -53,6 +55,7 @@ Indiana, IN 7.00
 Louisiana, LA 9.55
 New York, NY 8.53
 Washington, WA 9.40
+'''
 
 state = 'WA' #Provide states
 purchase_amount = 100 #amount of purchase
@@ -61,32 +64,26 @@ if state == 'CA':
     tax_amount = .0885
     total_cost = purchase_amount*(1+tax_amount)
     result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
-
 elif state == 'FL':
     tax_amount = .0702
     total_cost = purchase_amount*(1+tax_amount)
     result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
-
 elif state == 'IN':
     tax_amount = .0700
     total_cost = purchase_amount*(1+tax_amount)
     result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
-
 elif state == 'LA':
     tax_amount = .0955
     total_cost = purchase_amount*(1+tax_amount)
-    result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
-    
+    result = "Since you're from {}, your total cost is {}.".format(state, total_cost)   
 elif state == 'NY':
     tax_amount = .0853
     total_cost = purchase_amount*(1+tax_amount)
     result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
-
 elif state == 'WA':
     tax_amount = .0940
     total_cost = purchase_amount*(1+tax_amount)
     result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
-
 print(result)
 
 Since you're from WA, your total cost is 109.4.
