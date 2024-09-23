5d48bf642237d48ac753f094bbd25c07d118dc32
YvonneLipLim <164453695+YvonneLipLim@users.noreply.github.com>
2024-09-23 17:56:00 +0800

    Update 01. Arithmetic Operators Practice.py

--- 01. Arithmetic Operators Practice.py
+++ 01. Arithmetic Operators Practice.py
@@ -1,3 +1,17 @@
+"""
+Arithmetic operators are used to perform mathematical operations in Python.
+
+Arithmetic Functions:
+1. abs(): Returns the absolute value.
+2. round(): Rounds to the nearest integer.
+3. pow(): Raises one number to the power of another.
+
+An example will be:
+import math
+print(math.sqrt(16))  # 4.0
+"""
+
+
 #Q1. Calculate the average monthly electricity bill for a three-month period with bills of $28, $45, and $72. Write an expression to calculate the mean, and use print() to view the result.
 Jan = 28
 Feb = 45
@@ -5,8 +19,10 @@ Mar = 72
 Avg = round((Jan + Feb + Mar) / 3) #Round a floating-point value
 print('My average monthly electricity bill is $' + str(Avg) + '.') #Convert Avg from floating-point to string
 
+#Output:
 My average monthly electricity bill is $48.
 -----------------------------------------------------------------------------------------------------------------
+
 
 #Q2. A tiler needs to tile two rectangular areas of a floor. One area is 9 tiles wide by 7 tiles long, and the other is 5 tiles wide by 7 tiles long. Tiles are sold in packages of 6.
 # Part 1: Calculate the total number of tiles needed for both areas.
@@ -20,5 +36,6 @@ Total_Tiles_Purchase = 17 * 6
 Left_Over_Tiles = Total_Tiles_Purchase - Tiles_Needed
 print('Number of titles left over would be ' + str(Left_Over_Tiles))
 
+#Output:
 Total number of tiles needed for both areas would be: 98
 Number of titles left over would be 4
